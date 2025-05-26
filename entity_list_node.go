4e5eabed4b24c4baa831c810717c8aba77a6031f
Dmitry Caiman <dmitrycaiman@gmail.com>
2025-05-26 13:57:58 +0300

    Added linked list constructor.

--- entity_list_node.go
+++ entity_list_node.go
@@ -1,23 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
 type ListNode struct {
 	Val  int
 	Next *ListNode
 }
 
+func NewList(scheme string) *ListNode {
+	m, root := map[int]*ListNode{}, &ListNode{}
+	last := root
+	for _, v := range strings.Split(scheme, ",") {
+		n, err := strconv.Atoi(v)
+		if err != nil {
+			return nil
+		}
+		if existingNode, ok := m[n]; ok {
+			last.Next = existingNode
+			return root.Next
+		}
+		m[n] = &ListNode{Val: n}
+		last.Next = m[n]
+		last = last.Next
+	}
+	return root.Next
+}
+
 // Unpack формирует строку связного списка от текущего узла до хвоста либо до позиции циклирования.
 func (slf *ListNode) String() (result string) {
-	m, res, next := map[*ListNode]struct{}{}, "[", slf
+	if slf == nil {
+		return ""
+	}
+	m, res, next := map[*ListNode]struct{}{}, "", slf
 	for {
 		if _, ok := m[next]; ok {
-			return res + fmt.Sprintf("(%v)]", next.Val)
+			return res + fmt.Sprint(next.Val)
 		}
 		m[next] = struct{}{}
 		res += fmt.Sprintf("%v,", next.Val)
 		if next.Next == nil {
-			return res[:len(res)-1] + "]"
+			return res[:len(res)-1]
 		}
 		next = next.Next
 	}
---
+++ entity_list_node_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewList(t *testing.T) {
+	cases := []struct{ scheme, str string }{
+		{"1,3,5,7,9,1", "1,3,5,7,9,1"},
+		{"1,3,5,7,9,1", "1,3,5,7,9,1"},
+		{"1,3,5,7,9", "1,3,5,7,9"},
+		{"1,1,1", "1,1"},
+		{"", ""},
+	}
+	for _, c := range cases {
+		_ = c
+		l := NewList(c.scheme)
+		assert.Equal(t, c.str, l.String())
+	}
+}
