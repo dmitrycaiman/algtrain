e949100aa547fc4350c0588c201e4022a0d8fef6
Dmitry Caiman <dmitrycaiman@gmail.com>
2025-05-26 15:55:14 +0300

    Analitics for lc_0001_two_sum.

--- lc_0001_two_sum_test.go
+++ lc_0001_two_sum_test.go
@@ -6,17 +6,28 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+// https://leetcode.com/problems/two-sum/description/
 func Test_lc_0001_two_sum(t *testing.T) {
-	assert.ElementsMatch(t, []int{1, 3}, lc_0001_two_sum_1([]int{1, 4, 5, 3, 7, 1}, 7))
-	assert.ElementsMatch(t, []int{0, 1}, lc_0001_two_sum_1([]int{1, 4}, 5))
-	assert.ElementsMatch(t, []int{5, 6}, lc_0001_two_sum_1([]int{9, 9, 9, 9, 9, 9, 1}, 10))
-
-	assert.ElementsMatch(t, []int{1, 3}, lc_0001_two_sum_2([]int{1, 4, 5, 3, 7, 1}, 7))
-	assert.ElementsMatch(t, []int{0, 1}, lc_0001_two_sum_2([]int{1, 4}, 5))
-	assert.ElementsMatch(t, []int{0, 6}, lc_0001_two_sum_2([]int{9, 9, 9, 9, 9, 9, 1}, 10))
+	cases := []struct {
+		nums, output []int
+		target       int
+	}{
+		{nums: []int{1, 4, 5, 3, 7, 1}, target: 7, output: []int{1, 3}},
+		{nums: []int{1, 4}, target: 5, output: []int{0, 1}},
+		{nums: []int{8, 8, 8, 8, 8, 9, 1}, target: 10, output: []int{5, 6}},
+	}
+	for _, c := range cases {
+		assert.ElementsMatch(t, c.output, lc_0001_two_sum_map(c.nums, c.target))
+		assert.ElementsMatch(t, c.output, lc_0001_two_sum_brute(c.nums, c.target))
+	}
 }
 
-func lc_0001_two_sum_1(nums []int, target int) []int {
+// Создаём хеш-таблицу с целочисленными ключами и значениями. Ключ будет обозначать элемент входного массива,
+// а значение — позиция этого элемента во входном массиве.
+// Итерируемся по входному массиву. Вычисляем разность target и текущего элемента. Проверяем, есть ли в хеш-таблице значение по ключу, равному этой разности.
+// Если есть, то возвращаем индекс текущего элемента и значение по ключу, т.е. индекс "недостающего" элемента.
+// Если нет, то добавляем текущий элемент в хеш-таблицу по описанной схеме и продолжаем итерирование.
+func lc_0001_two_sum_map(nums []int, target int) []int {
 	m := map[int]int{}
 	for i, v := range nums {
 		if j, ok := m[target-v]; ok {
@@ -27,7 +38,7 @@ func lc_0001_two_sum_1(nums []int, target int) []int {
 	return nil
 }
 
-func lc_0001_two_sum_2(nums []int, target int) []int {
+func lc_0001_two_sum_brute(nums []int, target int) []int {
 	for i := range len(nums) - 1 {
 		for j := i + 1; j < len(nums); j++ {
 			switch {
