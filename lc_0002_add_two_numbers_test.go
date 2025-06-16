983a66ee0d5f25a49052c914d043023fb67a1c7a
Dmitry Caiman <dmitrycaiman@gmail.com>
2025-06-16 10:52:14 +0300

    Added analytics for lc_0002.

--- lc_0002_add_two_numbers_test.go
+++ lc_0002_add_two_numbers_test.go
@@ -53,20 +53,21 @@ func Test_lc_0002_add_two_numbers(t *testing.T) {
 			},
 		},
 	}
-	assert.True(t, lc_0002_CheckEquality(l3, lc_0002_add_two_numbers(l1, l2)))
-	assert.True(t, lc_0002_CheckEquality(l5, lc_0002_add_two_numbers(l3, l4)))
-	assert.True(t, lc_0002_CheckEquality(l3, lc_0002_add_two_numbers(nil, l3)))
-	assert.True(t, lc_0002_CheckEquality(l5, lc_0002_add_two_numbers(l5, nil)))
+	assert.True(t, lc_0002_checkEquality(l3, lc_0002_add_two_numbers(l1, l2)))
+	assert.True(t, lc_0002_checkEquality(l5, lc_0002_add_two_numbers(l3, l4)))
+	assert.True(t, lc_0002_checkEquality(l3, lc_0002_add_two_numbers(nil, l3)))
+	assert.True(t, lc_0002_checkEquality(l5, lc_0002_add_two_numbers(l5, nil)))
 	assert.Nil(t, lc_0002_add_two_numbers(nil, nil))
 }
 
+// Производим поразрядное сложение соответствующих значений списов, как "в столбик".
 func lc_0002_add_two_numbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	result := &ListNode{}
-	current := result
-	overflow := false
-	a, b := 0, 0
+	result := &ListNode{} // Корневой узел результата
+	current := result     // Текущий заполняемый узел результата
+	overflow := false     // Флаг переполнения предыдущего разряда
+	a, b := 0, 0          // Слагаемые текущего разряда
 	for {
-		switch {
+		switch { // Проверяем на пустоту следующие соответствующие узлы. Если они непустые, то берём их значения как слагаемые.
 		case l1 != nil && l2 != nil:
 			a, b = l1.Val, l2.Val
 			l1, l2 = l1.Next, l2.Next
@@ -76,7 +77,7 @@ func lc_0002_add_two_numbers(l1 *ListNode, l2 *ListNode) *ListNode {
 		case l1 == nil && l2 != nil:
 			a, b = 0, l2.Val
 			l2 = l2.Next
-		case l1 == nil && l2 == nil:
+		case l1 == nil && l2 == nil: // Если дальше "пути нет" (оба узла пустые), то учитываем флаг переполнения и выходим с результатом.
 			if overflow {
 				current.Next = &ListNode{Val: 1}
 			}
@@ -84,19 +85,19 @@ func lc_0002_add_two_numbers(l1 *ListNode, l2 *ListNode) *ListNode {
 		}
 		current.Next = &ListNode{}
 		current = current.Next
-		if overflow {
+		if overflow { // Если в предыдущем разряде было переполнение, то учитываем это в текущем значении одного из слагаемых.
 			overflow = false
 			a++
 		}
 		current.Val = a + b
-		if current.Val >= 10 {
+		if current.Val >= 10 { // Если случилось переполнение текущего разряда, то запоминаем это и помещаем в текущее значение только остаток.
 			overflow = true
 			current.Val -= 10
 		}
 	}
 }
 
-func lc_0002_CheckEquality(l1, l2 *ListNode) bool {
+func lc_0002_checkEquality(l1, l2 *ListNode) bool {
 	for {
 		switch {
 		case l1 == nil && l2 != nil:
