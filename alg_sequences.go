4e77e9c4eae8683b3d1cdcb2874bf3384b25c49f
Dmitry Caiman <dmitrycaiman@gmail.com>
2025-06-18 15:24:56 +0300

    Simplified fibonacciNumbers.

--- alg_sequences.go
+++ alg_sequences.go
@@ -19,14 +19,11 @@ func fibonacciNumbers(n int) int {
 	if n == 0 || n == 1 {
 		return n
 	}
-
-	n0, n1, n2 := 0, 1, 0
+	n1, n2 := 0, 1
 	for i := 2; i <= n; i++ {
-		n0 = n1 + n2
-		n1, n2 = n0, n1
+		n1, n2 = n2, n1+n2
 	}
-
-	return n1
+	return n2
 }
 
 // fibonacciNumbersMemo рассчитывает последовательность Фибоначчи рекурсивно с мемоизацией.
