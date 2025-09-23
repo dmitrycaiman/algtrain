d0310e72de2984b1b11c4dc200b18086b3cfc4c0
--get <dmitrycaiman@gmail.com>
2025-09-23 21:33:28 +0300

    lc_0074_search_a_2d_matrix

--- lc_0074_search_a_2d_matrix_test.go
+++ lc_0074_search_a_2d_matrix_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_lc_0374_search_a_2d_matrix(t *testing.T) {
+func Test_lc_0074_search_a_2d_matrix(t *testing.T) {
 	cases := []struct {
 		matrix [][]int
 		target int
@@ -22,14 +22,15 @@ func Test_lc_0374_search_a_2d_matrix(t *testing.T) {
 		{[][]int{{1, 2, 4}, {5, 7, 8}, {9, 9, 9}}, 10, false},
 	}
 	for _, c := range cases {
-		assert.Equal(t, c.result, lc_0374_search_a_2d_matrix(c.matrix, c.target))
+		assert.Equal(t, c.result, lc_0074_search_a_2d_matrix_1(c.matrix, c.target))
+		assert.Equal(t, c.result, lc_0074_search_a_2d_matrix_2(c.matrix, c.target))
 	}
 }
 
 // Выбираем ряд для поиска: если первый элемент ряда больше искомого, то нам подходит предыдущий ряд.
 // Далее осуществляем бинарный поиск в выбранном ряду. Если в процессе границы сошлись на одном элементе,
 // то сравниваем этот элемент с искомым. Если разошлись, то искомого элемента не существует.
-func lc_0374_search_a_2d_matrix(matrix [][]int, target int) bool {
+func lc_0074_search_a_2d_matrix_1(matrix [][]int, target int) bool {
 	row := len(matrix) - 1
 	for i := 1; i < len(matrix); i++ {
 		if matrix[i][0] > target {
@@ -53,6 +54,41 @@ func lc_0374_search_a_2d_matrix(matrix [][]int, target int) bool {
 		case s[m] < target:
 			a = m + 1
 		case s[m] == target:
+			return true
+		}
+	}
+}
+
+func lc_0074_search_a_2d_matrix_2(matrix [][]int, target int) bool {
+	n := -2
+	for i, v := range matrix {
+		if v[0] > target {
+			n = i - 1
+			break
+		}
+	}
+	switch n {
+	case -1:
+		return false
+	case -2:
+		n = len(matrix) - 1
+		if target > matrix[n][len(matrix[n])-1] {
+			return false
+		}
+	}
+	m := matrix[n]
+	for {
+		if len(m) == 0 {
+			return false
+		}
+		mid := len(m) / 2
+		val := m[mid]
+		switch {
+		case val > target:
+			m = m[:mid]
+		case val < target:
+			m = m[mid+1:]
+		default:
 			return true
 		}
 	}
