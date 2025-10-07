92ec2d2f3a8b988aceeb16f351d04049fecf1536
--get <dmitrycaiman@gmail.com>
2025-10-07 18:56:26 +0300

    lc_0228_summary_ranges

--- lc_0228_summary_ranges_test.go
+++ lc_0228_summary_ranges_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+// https://leetcode.com/problems/summary-ranges
 func Test_lc_0228_summary_ranges(t *testing.T) {
 	assert.Equal(t, []string{"1->5"}, lc_0228_summary_ranges([]int{1, 2, 3, 4, 5}))
 	assert.Equal(t, []string{"1->5", "7", "9", "11->12"}, lc_0228_summary_ranges([]int{1, 2, 3, 4, 5, 7, 9, 11, 12}))
@@ -14,10 +15,12 @@ func Test_lc_0228_summary_ranges(t *testing.T) {
 
 }
 
+// Два указателя. Первый движется до момента, пока не встретит число, которое отличается от следующего на единицу и более.
+// В этом случае записываем в массив результата промежуток: в зависимости от его размера — одно или два числа.
+// После сдвигаем второй указатель на позицию первого и повторяем так до конца массива.
 func lc_0228_summary_ranges(nums []int) []string {
 	result := []string{}
-	i := 0
-	for j := range len(nums) {
+	for i, j := 0, 0; j < len(nums); j++ {
 		if j == len(nums)-1 || nums[j+1]-nums[j] > 1 {
 			if j-i >= 1 {
 				result = append(result, fmt.Sprintf("%v->%v", nums[i], nums[j]))
